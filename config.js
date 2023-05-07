a694cffa16a5981c5e1f21e223093ffcdc9843a3
vishal-rana-official <122625999+vishal-rana-official@users.noreply.github.com>
2023-05-07 09:34:29 +0530

    updated

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    JWT_SECRET: 'DJDJ',
+    EMAIL: 'vishalrana10111.vr@gmail',
+    PASSWORD: "tbqnwkqqmirrkzyj",
+    GEMAIL:'vishalrana10888.vr@gmail.com',
+    GPASS:'vishalrana10888',
+    ATLAS_URI: 'mongodb+srv://admin:admin123@cluster0.equ4gaz.mongodb.net/test'
+}
